f6fcc63691a2409c3e814692f6e6e7eeb3007d30
marvin evins <maevins@cps.edu>
2018-12-01 11:45:27 -0600

    smoke test

---
+++ contracts/Election.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.4.24;
+
+contract Election{
+  //store candidate
+  //read candidate
+  string public candidate;
+ //constructor
+ function Election () public {
+     candidate = "Candidate 1";
+
+ }
+
+
+}
