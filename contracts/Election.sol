54b0fc97d6e746be3b7a16cdbd2ff009f64e7195
marvin evins <maevins@cps.edu>
2018-12-01 17:06:42 -0600

    added migrations and contracts

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,14 +1,29 @@
 pragma solidity ^0.4.24;
 
 contract Election{
+  //model  a candidate
+  struct Candidate{
+  uint id;
+  string name;
+  uint voteCount;
+}
   //store candidate
-  //read candidate
-  string public candidate;
+  mapping(uint=> Candidate) public candidates;
+  //fetch candidate
+// store candidates count
+uint public candidatesCount;
+
+
  //constructor
  function Election () public {
-     candidate = "Candidate 1";
-
+      addCandidate("Candidate 1");
+      addCandidate("Candidate 2");
  }
+
+  function addCandidate(string _name) private{
+    candidatesCount++;
+    candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+  }
 
 
 }
